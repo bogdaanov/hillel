6860dffd70f388cef4acac1ff4f856a2af74b390
bogdaanov <bogdanov.nikita.48@gmail.com>
2022-02-15 23:30:54 +0200

    update

---
+++ homework 5.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = 1
+c = 1
+while c <= a:
+    print(c)
+    b += 1
+    c = b ** 2
--- homewrok 4.py
+++ homewrok 4.py
@@ -1,6 +1,7 @@
+num = int(input("Enter you number: "))
 a = 2
 b = 1
-while a < 50:
+while a <= num:
     a *= 2
     b += 1
 print(b - 1, a // 2)
