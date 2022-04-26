ce8921abda85bbf98632d32449ef4d2189115348
bogdaanov <bogdanov.nikita.48@gmail.com>
2022-04-27 00:36:30 +0300

    update

--- homework for lesson 6.py
+++ homework for lesson 6.py
@@ -1,9 +1,12 @@
-text = '''Таким образом, когда целое число смешивается с числом с плавающей точкой,
-                                       как в предыдущем примере, то целое число преобразуется в значение с плавающей
-                                       точкой и математика с плавающей точкой выдает результат с плавающей точкой'''
+text = input().split()
+dict1 = {}
+for i in text:
+    if i not in dict1:
+        dict1[i] = 0
+    dict1[i] += 1
 
-dict = {}
-for i in text.split():
-    count = text.count(i)
-    dict[i] = count
-print(dict)
+sorted_dict = {}
+sorted_keys = sorted(dict1, key=dict1.get, reverse=True)
+for i in sorted_keys:
+    sorted_dict[i] = dict1[i]
+print(sorted_dict)
